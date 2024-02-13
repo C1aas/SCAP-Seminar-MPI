00284cf51761409ce2939abb6228f633d439d3fd
GOESTERN-0727141 <hpctraining47@gwdu102.global.gwdg.cluster>
2024-02-13 23:29:58 +0100

    fixed wrong run.sh

--- GameOfLife/run.sh
+++ GameOfLife/run.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name parallel_hello
 #SBATCH --partition medium
 #SBATCH --time 0:05:00
-#SBATCH --output slurm_output/job_output_%x_%j_$(date +%Y-%m-%d_%H-%M-%S).txt
+#SBATCH --output slurm_output/GoL_job_output.txt
 
 ##SBATCH--exclusive
 
@@ -31,6 +31,6 @@ export SCOREP_ENABLE_TRACING=true
 # mpiexec -np 1 time ./main 1000 10 -1
 
 # <grid_size:int> <total_iterations:int> <output_steps:int> <console_output:bool> <output_images:bool> <measure_time:bool>
-./bin/GameOfLife 1000 50 1 true false false
+./bin/GameOfLife 10000 50 -1 false false false
 
 # ./main 100 100 10 true false true
